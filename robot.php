0603c47a865c42b51d0de0cbfbc1a18022dc44c9
André <andrelokal@gmail.com>
2019-06-11 08:17:16 -0300

    Update robot.php

--- robot.php
+++ robot.php
@@ -17,17 +17,16 @@ foreach ($lines as $array) {
     $v = trim($arr[1]);
     $cmdline[][$k] = $v;
 }
-
 foreach ($cmdline as $key => $value) {
     foreach ($value as $cmd => $pos) {
-        if (is_string($cmd)) {
+        if (is_string($cmd) && count($MV) <= 10000) {
             $MV[][$cmd] = $pos; //movement
         } else {
-            $obs[$cmd] = $pos; //obstacle
+            if(count($obs) <= 10)
+                $obs[$cmd] = $pos; //obstacle
         }
     }
 }
-
 /*
  * processing the rows
  */
@@ -80,11 +79,8 @@ foreach ($MV as $kr => $vr) {
                     $x = $x + 1;
                     break;
             }
-
             $tr[] = sqrt((($x - 0) * ($x - 0)) + (($y - 0) * ($y - 0)));
             $tm[] = "($x,$y)";
-
-
         } else { //rotation
             if ($k == 'R') { //right
                 switch ($d) {
@@ -122,10 +118,8 @@ foreach ($MV as $kr => $vr) {
         }
     }
 }
-
 $unitDistance = max($tr);
 $key = array_search($unitDistance, $tr);
-
 echo "<br> maximum distance it ever got from (0, 0) is ".$tm[$key];
 echo "<br> ending at: ($x,$y)";
 echo "<br>  roughly ".round($unitDistance,2)." units away ";

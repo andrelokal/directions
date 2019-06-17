e8d94e097b79817464245d8a7bfdf170349f9c04
André <andrelokal@gmail.com>
2019-06-17 08:58:23 -0300

    Update index.php

--- robot_class/index.php
+++ robot_class/index.php
@@ -11,13 +11,13 @@ $obstacles = array();
 $rows = file('directions.txt');
 
 foreach ($rows as $row) {
-    $arr = explode(' ', $row);
-    $k = trim($arr[0]);
-    $v = trim($arr[1]);
-    if (!preg_match('/^[0-9]*$/', $k) && count($movements) <= 10000) {
-        $movements[][$k] = $v;
+    $array = explode(' ', $row);
+    $key = trim($array[0]);
+    $value = trim($array[1]);
+    if (!preg_match('/^[0-9]*$/', $key) && count($movements) <= 10000) {
+        $movements[][$key] = $value;
     } elseif (count($obstacles) <= 10) {
-        $obstacles[$k] = $v;
+        $obstacles[$key] = $value;
     }
 }
 
